031cf622a38348daeab1fc8ec621aa807cdc64a3
milanzagorac <zagoracm@gmail.com>
2020-11-08 14:48:52 -0500

    first cmt

---
+++ app.js
@@ -0,0 +1,41 @@
+//async await
+
+async function getData() {
+    const response = await axios.get('https://swapi.dev/api/planets/');
+    const { next, results } = response.data //destructure to assing variable to objects
+    for (let planet of results) { 
+        console.log(planet.name);
+    }
+    const responseTwo = await axios.get(next);
+    const resultsTwo = responseTwo.data.results;
+    for (let planet of resultsTwo) { 
+        console.log(planet.name);
+    }
+}
+
+//response is the promise object
+//then data is an object inside that object
+// then we iterate over the results of that data
+//then we can get the name property for the planet
+
+async function getSpaceData() {
+    const spaceResponse = await axios.get('https://api.spacexdata.com/v3/launches/upcoming');
+    // console.log(spaceResponse.data);
+    const ul = document.querySelector('#launches');
+    for (let launch of spaceResponse.data) {
+        const newLI = document.createElement('LI');
+        const mission = document.createElement('B');
+        mission.innerText = launch.mission_name;
+        newLI.append(mission);
+        newLI.innerHTML += ` - ${launch.rocket.rocket_name}`
+        ul.append(newLI);
+        // console.log(launch.mission_name);
+        // console.log(launch.flight_number);
+        // console.log(launch.rocket.rocket_name);
+    }
+};
+
+const btn = document.querySelector('#getLaunches');
+btn.addEventListener('click', getSpaceData);
+
+// the above example will show the launch mission name and the rocket being used
